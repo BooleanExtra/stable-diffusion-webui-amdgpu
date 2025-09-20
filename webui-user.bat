f42c4d378eec9a80a2392cb79c9d0b7b0d60a501
getBoolean <19920697+getBoolean@users.noreply.github.com>
2025-09-19 20:01:42 -0500

    web ui config for amd pc

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\Boolean\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--use-zluda --update-check --skip-ort --skip-python-version-check --no-half --listen
 
 call webui.bat
