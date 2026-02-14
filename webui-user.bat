0e3ce3449a335a01334f55ca6565b3e9041d54d3
getBoolean <19920697+getBoolean@users.noreply.github.com>
2026-02-14 00:35:30 -0600

    change python dir

--- webui-user.bat
+++ webui-user.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PYTHON="C:\Users\Boolean\AppData\Local\Programs\Python\Python310\python.exe"
+set PYTHON="C:/Users/Boolean/.pyenv/pyenv-win/versions/3.10.6/python.exe"
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=--use-zluda --update-check --skip-ort --skip-python-version-check --no-half --listen
