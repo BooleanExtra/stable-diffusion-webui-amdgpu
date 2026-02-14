a77d2f102563c038ca71da509a94ae8cbd4936d8
getBoolean <19920697+getBoolean@users.noreply.github.com>
2026-02-14 01:49:37 -0600

    Set hip path so it doesnt use latest

--- webui-user.bat
+++ webui-user.bat
@@ -4,5 +4,6 @@ set PYTHON="C:/Users/Boolean/.pyenv/pyenv-win/versions/3.10.6/python.exe"
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=--use-zluda --update-check --skip-ort --skip-python-version-check --no-half --listen
+set HIP_PATH="C:/Program Files/AMD/ROCm/6.2"
 
 call webui.bat
